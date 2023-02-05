b50e731e73f92433cf1a7adb9e18bb04fc9f093f
Ruhul Muazzam Rafat <rmrafat420@gmail.com>
2023-02-05 17:27:14 +0600

    database added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 
 require('dotenv').config()
 const port = process.env.PORT || 5000
@@ -9,7 +11,19 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+
+
+
+
+const uri = `mongodb+srv:${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lfwjozb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+
 async function run(){
+    
     try{
 
 
