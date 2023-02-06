c07802bde6e502b4b48c67bb10bc74a15c327c94
Ruhul Muazzam Rafat <rmrafat420@gmail.com>
2023-02-06 22:42:43 +0600

    image added

--- index.js
+++ index.js
@@ -19,9 +19,6 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
-
-
-
 async function run(){
     
     try{
